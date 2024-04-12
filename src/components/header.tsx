76ef722d290d549a22271b71818e751fbf67f9d5
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2024-04-12 19:12:17 -0400

    more setup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,32 +11,17 @@ export function Header() {
         <div className="flex items-center space-x-4">
           <Link className="flex items-center space-x-2" href="#">
             {/* <FlagIcon className="w-12 h-12" /> */}
-            <img src="/logo.png" alt="Logo" className="w-12 h-12" />
+            <img
+              src="/Revu-Landing-Page
+/logo.png"
+              alt="Logo"
+              className="w-12 h-12"
+            />
             <span className="text-2xl font-bold tracking-tighter">Revu</span>
           </Link>
         </div>
         <nav className="hidden md:flex items-center space-x-4"></nav>
       </div>
     </header>
-  );
-}
-
-function FlagIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/s=vg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
-      <line x1="4" x2="4" y1="22" y2="15" />
-    </svg>
   );
 }
