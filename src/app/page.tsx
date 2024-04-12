e05ab005bb1ce7c3fa3addfbba318ed821d13478
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2024-04-12 19:05:41 -0400

    setup landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Header } from "@/components/header";
+import { LandingPage } from "@/components/landing-page";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <Header />
+      <LandingPage />
+      <div className="flex justify-center">
+        <video
+          src="/Revu Demo.mp4"
+          controls
+          style={{ width: "800px", height: "600px" }}
+        />
+      </div>
+    </main>
+  );
+}
